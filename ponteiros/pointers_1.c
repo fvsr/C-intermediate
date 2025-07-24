a5c0f85a6af7d79f60b370f65800fb71d8351898
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-07-24 19:28:15 +0000

    Ponteiros

--- ponteiros/pointers_1.c
+++ ponteiros/pointers_1.c
@@ -3,12 +3,19 @@
 int main(){
     int numero = 10; //variável normal
     int *ponteiro;   //declaração de um ponteiro
+                     // o tipo desta variável é 'int *'
 
     ponteiro = &numero; //inicia a variável "ponteiro" com o endereço de "numero"
+                        //aqui o sinal & é o operador de endereço
 
     printf("O valor da variável 'numero' é: %d\n", numero);
     printf("O endereço da variável 'numero' é: %p\n", ponteiro);
-    printf("O valor apontado por 'ponteiro' é: %d\n", *ponteiro);
+    printf("O valor apontado por 'ponteiro' é: %d\n", *ponteiro); //acesso indireto
+    //aqui o sinal % é especificador de formato
+    //%p é usado para ponteiros
+    //se uma função recebe um ponteiro está sendo passado um valor por referência
+    //o oposto é passar um valor por cópia, onde o valor é copiado para outra variável dentro da função
+    //o ponteiro evita a cópia de dados 
 
     return 0;
 }
