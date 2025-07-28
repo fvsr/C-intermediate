ac0574797b0f9afad173bed6eae5051ce899f3f6
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-07-28 04:15:41 +0000

    Structs

--- 4-structs/struct_aninhada.c
+++ 4-structs/struct_aninhada.c
@@ -5,7 +5,7 @@
 //onde passamos para a função o ponteiro para uma struct.
 //Isso é útil para evitar a cópia de grandes estruturas de dados,
 //o que pode ser ineficiente em termos de desempenho e uso de memória.
-//Usando o ponteiro para uma struct, pasamos o endereço do início da struct,
+//Usando o ponteiro para uma struct, passamos o endereço do início da struct,
 //e podemos modificar diretamente os dados da struct original,
 //sem precisar fazer uma cópia dela. 
 
@@ -30,17 +30,22 @@ struct Aluno{
 //função para cadastrar um aluno
 //Aqui estamos passando uma struct por referência, um ponteiro para uma struct.
 //As variáveis nome e professor pertencem à struct Disciplina,
-//e tem que ser passadas por refência porque são arrays de caracteres,
+//e têm que ser passadas por refência porque são arrays de caracteres,
 //ou seja, strings em C, desse modo não precisamos copiar toda a string,
 //apenas passamos o endereço de memória onde o primeiro caractere da string está armazenado. 
-//Verificar o porquê de usar const aqui.
+
+//O uso "const" aqui significa que as strings passadas para a função não serão modificadas,
+//ou seja, a função não irá alterar o conteúdo das strings.
+//Isso é uma boa prática para evitar modificações acidentais e melhorar a segurança do código
+//e a legibilidade, indicando que essas strings são apenas para leitura dentro da função.
+//As variáveis que não usam "const" subentende-se que poderão sofrer atualização em algum momento,
+//como cargaHoraria, idade e matrícula.
 void cadastrarDisciplina(struct Disciplina *disciplina, const char *nome, const char *professor, int cargaHoraria){
     strcpy(disciplina->nome, nome);
     strcpy(disciplina->professor, professor);
     disciplina->cargaHoraria = cargaHoraria;
 }
 
-//Verificar o porquê de usar const aqui.
 //função cadastrar aluno
 void cadastrarAluno(struct Aluno *aluno, const char *nome, int idade, int matricula){
     strcpy(aluno->nome, nome);
@@ -48,6 +53,7 @@ void cadastrarAluno(struct Aluno *aluno, const char *nome, int idade, int matric
     aluno->matricula = matricula;
     
     //cadastrando disciplinas
+    //função dentro de outra função
     cadastrarDisciplina(&aluno->disciplinas[0], "Matemática", "Prof. João", 60);
     cadastrarDisciplina(&aluno->disciplinas[1], "Física", "Prof. Maria", 45);
     cadastrarDisciplina(&aluno->disciplinas[2], "Química", "Prof. José", 30);
@@ -62,19 +68,25 @@ void exibirAluno(struct Aluno *aluno){
     
     printf("Disciplinas:\n");
     for(i = 0; i < 3; i++){
-        printf("Disciplina  %d:\n", i + 1);
-        printf("  Nome: %s\n", aluno->disciplinas[i].nome);
-        printf("  Professor: %s\n", aluno->disciplinas[i].professor);
-        printf("  Carga Horária: %d\n", aluno->disciplinas[i].cargaHoraria);
+        printf("Disciplina %d:\n", i + 1);
+        printf("  Nome: %s.\n", aluno->disciplinas[i].nome);
+        printf("  Professor: %s.\n", aluno->disciplinas[i].professor);
+        printf("  Carga Horária: %d.\n", aluno->disciplinas[i].cargaHoraria);
     }
 }
 
 //função principal
 int main(){
     //declaração de uma variável do tipo "struct Aluno"
-    struct Aluno aluno;
+    struct Aluno aluno;//A variável aluno é do tipo struct Aluno,
+                       //e o ponteiro struct Aluno *aluno não tem relação com ela,
+                       //a não ser pelo nome ser igual.
+                       //Elas têm escopos diferentes, uma em "main" e outra na função cadastrarAluno,
+                       //além disso a primeira é uma struct e a segunda um ponteiro para struct.
 
-    cadastrarAluno(&aluno, "Alana Lima", 20, 123456);
+    cadastrarAluno(&aluno, "Alana Lima", 20, 123456); //Aqui é enviado endereço de "aluno",
+                                                      //ou seja, um ponteiro para a variável struct Aluno aluno,
+                                                      //visto que a função cadastrarAluno espera um ponteiro para struct Aluno.
     exibirAluno(&aluno);
 
     return 0;
