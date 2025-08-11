e31d3bf7b6221bd4d51331f1131d5e980240f4da
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-08-11 16:46:11 +0000

    Listas lineares

---
+++ 8-listas-lineares/estruturas-dados-lineares.c
@@ -0,0 +1,31 @@
+//Estruturas de dados lineares
+
+//São estruturas de dados que organizam os elementos de forma sequencial, um após o outro.
+
+//Quatro tipos de estruturas de dados lineares:
+//vetores, listas, pilhas e filas.
+
+//Vetores funcionam como um armário com caixas numeradas,
+//onde cada conteúdo pode ser acessado pelo endereço da caixa.
+//Listas funcionam como uma corrente, ou um colar de pérolas, 
+//para se chegar no último elo é preciso passar por todos os outros.
+//Outros exemplos são o histórico do navegador de internet
+//Pilhas funcionam como uma pilha de pratos, o primeiro a entrar é o último a sair,
+//(LIFO, last in, first out).
+//Outro exemplo é a função "ctrl z" em um editor de texto.
+//Filas funcionam como uma fia do caixa de supermercados, o primeiro a entrar é o primeiro a sair
+//(FIFO, first in, first out).
+
+//Exemplos de estrutura de dados linear:
+/*
+struct Tarefa{
+    char descrição[100];
+    int prioridade;
+    struct Tarefa *próxima //ponteiro para próxima tarefa
+};
+
+struct No {
+    int dado; //o dado que guardamos, por exemplo um número
+    struct No *proximo; //acessa o próximo dado
+}
+*/
