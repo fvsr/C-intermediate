74aa7ea6e3785ba52c873d72c3c86ffe1c68738a
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-08-12 00:21:38 +0000

    Estruturas não lineares

---
+++ 9-estruturas-nao-lineares/estruturas-nao-lineares.c
@@ -0,0 +1,27 @@
+//Estruturas de dados não lineares
+
+//São estruturas de dados onde cada ítem pode ser conectado a vários outros ítens,
+//formando hierarquias, como uma ávore genealógica,
+//ou redes complexas, como as amizades em uma rede social.
+//As estruturas de dados não lineares são aquelas que representam conexões múltiplas.
+
+//Dois tipo de estruturas de dados não lineares: árvores e grafos.
+
+//Árvores.
+//Um exemplo de estrutura de dados não linear em árvore é a estrutura de pastas de arquivos do computador.
+//Existe uma pasta principal, a raiz, e pastas são colocadas dentro de pastas, como no desenho abaixo.
+/*
+                         raiz                           nível 0
+                 /                 \                  
+            pasta1                    pasta2            nível 1
+           /     \                    /    \
+     pasta1.1   pasta1.2      pasta2.1     pasta2.2     nível 2
+       /
+ folha1.1.1
+ folha1.1.2
+ folha1.1.3
+*/
+
+//Grafos
+//Ao contrário das árvores, os nós não tem hierarquia, e qualquer nó pode se conectar a qualquer outro nó.
+//Um exemplo é a rede de amizades de uma uma rede social.
