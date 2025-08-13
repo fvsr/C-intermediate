38b71734e2370a2f36fe3c30cf91b5f44899a0d1
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-08-13 18:21:04 +0000

    Ponteiros

---
+++ 1-ponteiros/ponteiro-struct.c
@@ -0,0 +1,33 @@
+//Ponteiro para uma struct
+
+//Quando usamos ponteiros para structs, cada membro da struct é acessado pelo operador seta.
+//Em uma struct que não é pontero, cada mem bro da struct é acessado pelo elemento ponto.
+
+#include <stdio.h>
+#include <stdlib.h> //a função malloc usa esta biblioteca
+
+typedef struct {
+
+    char nome[50];
+    int idade;
+
+} Pessoa;
+
+int main () {
+    //p é um ponteiro para uma struct do tipo Pessoa
+    Pessoa *p = (Pessoa*)malloc(sizeof(Pessoa));
+
+    //preenchendo a struct apontada com dados
+    printf("Digite seu nome: ");
+    scanf("%s", p->nome); //não se usa o operador de endereço
+                          //por que o nome da string já é o endereço
+
+    printf("Digite sua idade: ");
+    scanf("%d", &p->idade); //aqui se usa o operador de endereço
+
+    printf("%s tem %d anos.\n",p->nome, p->idade);
+
+    free(p);
+    return 0;
+}
+
