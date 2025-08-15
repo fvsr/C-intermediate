27d3235f758152099002c03f0c8a7aa92f239413
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-08-15 02:09:48 +0000

    Modularização

---
+++ 5-modularizacao/arquivos-cabecalho.c
@@ -0,0 +1,21 @@
+//Arquivos de cabeçalho
+
+//A funções são blocos de código que realizam uma tarefa específica.
+//São o primeiro nível de modularização.
+
+//O próximo de de modularização são os arquivos de cabeçalho.
+//O arquivo de cabeçalho, com extensão ".h" funciona como um índice,
+//ou um cardápio de funções e estruturas previamente programadas,
+//que estão disponíveis naquele módulo,
+//mas não mostram como essas estruturas funcionam,
+//o funcionamento das funções e estruturas são descritos nos arquivos ".c".
+//A declaração está no arquivo ".h" e a implementação estáno arquivo ".c".
+//Em "stdio.h" estão as funções "printf" e "scanf".
+//Em "stdlib.h" estão as funções "malloc", "calloc" e "free".
+//Os arquivos ".h" definem o "o que", enquanto os arquivos ".c" definem o "como".
+
+//Abstração
+//Abstrair significa esconder os detalhes e se ater apenas ao essencial.
+
+//Benefícios da modularização: manutenção, reutilização do código,
+//trabalho em equipe, facilidade de teste.
